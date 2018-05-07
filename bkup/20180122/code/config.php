0c21e1300e6f12bbfaffd9ac6712ecf30ae009f6
Eric Mikan <eric.mikan@synergetictech.com>
2018-05-07 11:10:11 -0400

    clen up 2

--- bkup/20180122/code/config.php
+++ bkup/20180122/code/config.php
@@ -5,20 +5,13 @@
 			"appLoginPage" => "/login.php"
 		];
 		public $db = [
-			"user" => "empulsemysore",
-			"pwd" => "empulse_70",
-			"host" => "atg-empulse.cbzzjswvipbd.us-west-2.rds.amazonaws.com",
+			"user" => "xxxxxxxxx",
+			"pwd" => "xxxxxxxxx",
+			"host" => "xxxxxxxxx",
 			"name" => "atg_dashboard"
 		];
 	};
 	
 	$GLOBALS['CONFIG'] = new appConfig();
 	
-	//$CONFIG_appLoginPage = "/login.php";
-	
-	//$CONFIG_db_user = "empulsemysore";
-	//$CONFIG_db_pwd = "empulse_70";
-	//$CONFIG_db_host = "atg-empulse.cbzzjswvipbd.us-west-2.rds.amazonaws.com"; 
-	//$CONFIG_db_name = "atg_dashboard";
-	//$conn = mysqli_connect($hostname, $username, $password, $dbname); 
 ?>
